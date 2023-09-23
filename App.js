0d2188ad843f58d0bb9ebd09363ca91329d06c25
Manish Kumar <manish.kumar@tradesocio.com>
2023-09-23 18:14:52 +0530

    removed keys

--- App.js
+++ App.js
@@ -8,7 +8,7 @@ import {MovieContext} from './Context';
 
 const App = () => {
   return (
-    <StripeProvider publishableKey="pk_test_51Ns2dOSJwOwrFd1LKWnUcnOB4Snuq35c2eNNZE2bs6fdeq0fI220Eu5h59NHm4VOZDYqyHXQcPXZD9G2BJ3GE5ZG00gihX0ecg">
+    <StripeProvider publishableKey="getting from the stripe website">
       <MovieContext>
         <StackNavigator />
       </MovieContext>
